2500af3e21b498752987c1d2dad4fc781d6d2bb2
valdir-alves3000 <valdiralves3000@gmail.com>
2023-12-17 12:29:43 -0300

    feat: add engine

---
+++ js/engine.js
@@ -0,0 +1,7 @@
+import { AddEventListener } from "./addEventListener.js";
+import { createStars } from "./createStars.js";
+
+const addEventListener = new AddEventListener();
+
+addEventListener.add();
+createStars(500);
