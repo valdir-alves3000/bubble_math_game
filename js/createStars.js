85286241532f6f3170411c5d48c72d22caacd86c
valdir-alves3000 <valdiralves3000@gmail.com>
2023-12-18 10:19:35 -0300

    chore: optimize star generation for dynamic screen sizes

--- js/createStars.js
+++ js/createStars.js
@@ -1,18 +1,18 @@
 export function createStars(quantity) {
   const body = document.querySelector("body");
+  const width = body.offsetWidth;
+  const height = body.offsetHeight;
 
-  for (let i = 0; i < quantity
-; i++) {
+  for (let i = 0; i < quantity; i++) {
     const star = document.createElement("div");
     const size = Math.random() * 3;
     star.className = "star";
     star.style.width = `${size}px`;
     star.style.height = `${size}px`;
-    star.style.left = `${Math.random() * 1200}px`;
-    star.style.top = `${Math.random() * 900}px`;
+    star.style.left = `${Math.random() * width}px`;
+    star.style.top = `${Math.random() * height}px`;
     star.style.animationDelay = `-${size * 3}s`;
 
     body.appendChild(star);
   }
 }
-
